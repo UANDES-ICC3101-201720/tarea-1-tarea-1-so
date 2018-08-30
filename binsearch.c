580d8ac5f42f46de249a896a0dbab7baa4444ae9
IgnacioFigueroa <ijfigueroa@miuandes.cl>
2018-08-30 19:01:46 -0300

    avance

--- binsearch.c
+++ binsearch.c
@@ -35,9 +35,9 @@ int serial_binsearch(int arr[], int l, int r, int x) {
 int parallel_binsearch(void * arguments) {
 
 	struct arg_struct *args = arguments;
-	printf("%s%d\n", "arg2: ", args->arg2);
-	printf("%s%d\n", "arg3: ", args->arg3);
-	printf("%s%d\n", "arg4: ", args->arg4);
+	printf("%s%d\n", "arg2", args->arg2);
+    printf("%s%d\n", "arg3", args->arg3);
+    printf("%s%d\n", "arg4", args->arg4);
 	if (args->arg3>=1){
 		int mid = args->arg2 + (args->arg3-1)/2;
 		struct arg_struct args1;
@@ -53,10 +53,11 @@ int parallel_binsearch(void * arguments) {
 		args2.arg4 = args->arg4;
 		if(args->arg1[mid]!=args->arg4){
 			pthread_t thread1, thread2;
-			pthread_create(&thread1, NULL, parallel_binsearch,&args1);
-			pthread_create(&thread2, NULL, parallel_binsearch,&args2);
+			pthread_create(&thread1, NULL, parallel_binsearch, &args1);
+			pthread_create(&thread2, NULL, parallel_binsearch, &args2);
 		}
 		if(args->arg1[mid] == args->arg4) return mid;
+		return 0;
 	}
     
 }
@@ -110,7 +111,7 @@ int main(int argc, char** argv) {
     if(dtgnid > 0){
     	printf("%s%d\n","Datagen PID: ", dtgnid );
     	char * name= "./datagen";
-    	execvp(name, &name);
+    	//execvp(name, &name);
     }
     else if (dtgnid<0){
     	fprintf(stderr, "%s\n", "Can't create Datagen as child process");
@@ -129,7 +130,8 @@ int main(int argc, char** argv) {
     prueba.arg2 = 0;
     prueba.arg3 =999;
     prueba.arg4 = 708;
-    printf("%s: %d\n","Se encuentra en la posicion:",parallel_binsearch(&arreglo));
+    
+    printf("%s: %d\n","Se encuentra en la posicion:",parallel_binsearch(&prueba));
 	pthread_t serial, parallel;
 	//pthread_create(&serial, NULL, (serial_binsearch), NULL); // El ultimo parametro esta por ver porque son los argumento de la fn
 	//pthread_create(&parallel, NULL, (parallel_binsearch), );//Lo mismo de arriba
