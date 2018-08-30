552904ff76f5e34ca4b1934193fa48ec433b4f50
IgnacioFigueroa <ijfigueroa@miuandes.cl>
2018-08-29 21:55:29 -0300

    Avance parallel binsearch

--- binsearch.c
+++ binsearch.c
@@ -15,6 +15,12 @@
 #define _POSIX_C_Source 2
 
 // TODO: implement
+struct arg_struct{
+	int arg2;
+	int arg3;
+	int arg4;
+	int* arg1;
+};
 int serial_binsearch(int arr[], int l, int r, int x) {
 	if(r>=1){
 		int mid = l +(r-l)/2;
@@ -26,7 +32,28 @@ int serial_binsearch(int arr[], int l, int r, int x) {
 }
 
 // TODO: implement
-int parallel_binsearch(void *arg) {
+int parallel_binsearch(void * arguments) {
+	struct arg_struct *args = arguments;
+	if (args->arg3>1){
+		int mid = args->arg2 + (args->arg3-1)/2;
+		struct arg_struct args1;
+		args1.arg1 = args->arg1;
+		args1.arg2 = args->arg2;
+		args1.arg3 = mid-1;
+		args1.arg4 = args->arg4;
+		
+		struct arg_struct args2;
+		args2.arg1 = args->arg1;
+		args2.arg2 = mid+1;
+		args2.arg3 = args->arg3;
+		args2.arg4 = args->arg4;
+		if(args->arg1[mid]!=args->arg4){
+			pthread_t thread1, thread2;
+			pthread_create(&thread1, NULL, parallel_binsearch,&args1);
+			pthread_create(&thread2, NULL, parallel_binsearch,&args2);
+
+		}
+	}
     return 0;
 }
 
@@ -76,21 +103,21 @@ int main(int argc, char** argv) {
     /* TODO: start datagen here as a child process. */
     int dtgnid = fork();
     
-    if(dtgnid >= 0){
+    if(dtgnid > 0){
     	printf("%s%d\n","Datagen PID: ", dtgnid );
     	char * name= "./datagen";
     	execvp(name, &name);
     }
     else if (dtgnid<0){
-    	fprintf(stderr, "%s\n", "Can't create Datagen as ");
+    	fprintf(stderr, "%s\n", "Can't create Datagen as child process");
     }
 
     /* TODO: implement code for your experiments using data provided by datagen and your
      * serial and parallel versions of binsearch.
      * */
 	pthread_t serial, parallel;
-	//pthread_create(&serial, NULL, int (&serial_binsearch)(int *, int, int, int), NULL); // El ultimo parametro esta por ver porque son los argumento de la fn
-	pthread_create(&parallel, NULL, int(&parallel_binsearch)(void *), void * arg);//Lo mismo de arriba
+	pthread_create(&serial, NULL, (serial_binsearch), NULL); // El ultimo parametro esta por ver porque son los argumento de la fn
+	pthread_create(&parallel, NULL, (parallel_binsearch), 1);//Lo mismo de arriba
 	pthread_join(serial, NULL);
 	pthread_join(parallel, NULL);
     /* TODO: connect to datagen and ask for the necessary data in each experiment round.
