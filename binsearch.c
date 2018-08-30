39da0d1dea627afc23fa22ae4e71a2321a5a5331
IgnacioFigueroa <ijfigueroa@miuandes.cl>
2018-08-29 22:28:36 -0300

    Avance

--- binsearch.c
+++ binsearch.c
@@ -33,8 +33,12 @@ int serial_binsearch(int arr[], int l, int r, int x) {
 
 // TODO: implement
 int parallel_binsearch(void * arguments) {
+
 	struct arg_struct *args = arguments;
-	if (args->arg3>1){
+	printf("%s%d\n", "arg2: ", args->arg2);
+	printf("%s%d\n", "arg3: ", args->arg3);
+	printf("%s%d\n", "arg4: ", args->arg4);
+	if (args->arg3>=1){
 		int mid = args->arg2 + (args->arg3-1)/2;
 		struct arg_struct args1;
 		args1.arg1 = args->arg1;
@@ -51,10 +55,10 @@ int parallel_binsearch(void * arguments) {
 			pthread_t thread1, thread2;
 			pthread_create(&thread1, NULL, parallel_binsearch,&args1);
 			pthread_create(&thread2, NULL, parallel_binsearch,&args2);
-
 		}
+		if(args->arg1[mid] == args->arg4) return mid;
 	}
-    return 0;
+    
 }
 
 int main(int argc, char** argv) {
@@ -115,11 +119,22 @@ int main(int argc, char** argv) {
     /* TODO: implement code for your experiments using data provided by datagen and your
      * serial and parallel versions of binsearch.
      * */
+
+    struct arg_struct prueba;
+    int arreglo[1000];
+    for (int i =0; i<1000;i++){
+    	arreglo[i]=i;
+    }
+    prueba.arg1 = arreglo;
+    prueba.arg2 = 0;
+    prueba.arg3 =999;
+    prueba.arg4 = 708;
+    printf("%s: %d\n","Se encuentra en la posicion:",parallel_binsearch(&arreglo));
 	pthread_t serial, parallel;
-	pthread_create(&serial, NULL, (serial_binsearch), NULL); // El ultimo parametro esta por ver porque son los argumento de la fn
-	pthread_create(&parallel, NULL, (parallel_binsearch), 1);//Lo mismo de arriba
-	pthread_join(serial, NULL);
-	pthread_join(parallel, NULL);
+	//pthread_create(&serial, NULL, (serial_binsearch), NULL); // El ultimo parametro esta por ver porque son los argumento de la fn
+	//pthread_create(&parallel, NULL, (parallel_binsearch), );//Lo mismo de arriba
+	//pthread_join(serial, NULL);
+	//pthread_join(parallel, NULL);
     /* TODO: connect to datagen and ask for the necessary data in each experiment round.
      * Create a Unix domain socket with DSOCKET_PATH (see const.h).
      * Talk to datagen using the messages specified in the assignment description document.
